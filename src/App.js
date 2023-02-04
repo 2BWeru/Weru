956c3e5fdbe325fc7bac0f03396cef43c1193388
Wamaitha <wamaithaweru19@gmail.com>
2023-02-04 18:05:37 +0300

    import Service Component,Import style css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './Components/About/about';
+import Header from './Components/Header/header';
+import Contact from './Components/Contact/contact';
+import Services from './Components/Services/services';
+import Projects from './Components/Projects/projects';
+// import "./static/css/owl.carousel.min.css";
+// import "./static/css/owl.theme.default.min.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <About />
+      <Services />
+      <Projects />
+      <Contact />
     </div>
   );
 }
