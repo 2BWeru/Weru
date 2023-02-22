3e620056e66e7430cf3e0f43caa60665489c6419
Wamaitha <wamaithaweru19@gmail.com>
2023-02-22 12:52:36 +0300

    edit background color,styling components for paragraghs,border ...

--- src/Components/Contact/contact.js
+++ src/Components/Contact/contact.js
@@ -1,62 +1,55 @@
 import React from "react";
+import "./contact.css";
+import {MdEmail} from "react-icons/md";
+import {BiPhoneCall} from "react-icons/bi"
 
-
+// BsLinkedin
 
 
 function contact() {
+   const date = new Date();
+   
   return (
     <>
       {/* <!-- choose section start --> */}
       <div class="choose_section layout_padding">
          <div class="container">
-            <h1 class="choose_taital">Why Choose Us</h1>
-            <p class="choose_text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All </p>
-            <div class="read_bt_1"><a href="#">Read More</a></div>
-            <div class="newsletter_box">
-               <h1 class="let_text">Let Start Talk with Us</h1>
-               <div class="getquote_bt"><a href="#">Get A Quote</a></div>
+            <div>
+            <h1 class="choose_taital">Are You Interested ?</h1>
+            <p class="choose_text">Whether you have an interesting project or just want to say hi,<br /> feel free to send me a message.</p>
+            <div class="read_bt_1"><a href="#">Message Me </a></div>
+            <br />
+            <br />
+            <br />
+
             </div>
+            <div className="contact-icon">
+            <div class="row">
+                  <div class="col-6">
+                  <div className="con">
+                     <BiPhoneCall className="icon-con"/>
+                     <br />
+                     <p>+254113327116</p>
+                  </div>
+                  </div>
+                  <div class="col-6">
+                  <div className="con">
+                     <MdEmail className="icon-con" />
+                     <br />
+                     <br />
+                     <a href="mailto:wamaithaweru19@gmail.com"><p>wamaithaweru19@gmail.com</p></a>
+                  </div>
+                  </div>
+               </div>
+            </div>
+            {/* <div class="newsletter_box">
+               <h1 class="let_text">Let's Work Together</h1>
+               <div class="getquote_bt"><a href="#">Get In Touch </a></div>
+            </div> */}
          </div>
       </div>
       {/* <!-- choose section end --> */}
-      {/* <!-- footer section start --> */}
-      <div class="footer_section layout_padding">
-         <div class="container">
-            <div class="input_btn_main">
-               <input type="text" class="mail_text" placeholder="Enter your email" name="Enter your email" />
-               <div class="subscribe_bt"><a href="#">Subscribe</a></div>
-            </div>
-            <div class="location_main">
-               <div class="call_text">
-                 {/* <img src="images/call-icon.png"> */}
-
-                 </div>
-               <div class="call_text"><a href="#">Call +01 1234567890</a></div>
-               <div class="call_text">
-                 {/* <img src="images/mail-icon.png"> */}
-                  
-                 </div>
-               <div class="call_text"><a href="#">demo@gmail.com</a></div>
-            </div>
-            <div class="social_icon">
-               <ul>
-                  <li><a href="#">
-                    {/* <img src="images/fb-icon.png"> */}
-                  </a></li>
-                  <li><a href="#">
-                    {/* <img src="images/twitter-icon.png"> */}
-                  </a></li>
-                  <li><a href="#">
-                    {/* <img src="images/linkedin-icon.png"> */}
-                  </a></li>
-                  <li><a href="#">
-                    {/* <img src="images/instagram-icon.png"> */}
-                  </a></li>
-               </ul>
-            </div>
-         </div>
-      </div>
-      {/* <!-- footer section end --> */}
+      
       {/* <!-- copyright section start --> */}
       <div class="copyright_section">
          <div class="container">
